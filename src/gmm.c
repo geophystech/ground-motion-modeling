b811c8a9e8f2d31a88d3d264fa9c7fc51a5687ee
Andrey Stepnov <a.stepnov@geophystech.ru>
2017-05-12 14:33:40 +0000

    Fix makefile

--- src/gmm.c
+++ src/gmm.c
@@ -13,7 +13,7 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-#include "GeographicLib-1.48/legacy/C/geodesic.h"
+#include "../third_party/GeographicLib-1.48/legacy/C/geodesic.h"
 #include "gmm.h"
 #include "stdio.h"
 
