c5ab4640e4a2cb90e58be0ab63d9afd8b5ad86b3
Andrey Stepnov <a.stepnov@geophystech.ru>
2017-05-16 14:43:28 +0000

    AS2008 GMPE, fix structs, print functions #4

--- src/gmm.h
+++ src/gmm.h
@@ -18,7 +18,6 @@
 //****************
 #define GRID_COLUMNS 3
 
-
 //****************
 //*  INCLUDES    *
 //****************
@@ -42,7 +41,7 @@ int64_t grid_size_global;
 typedef struct {
   double lat;
   double lon;
-  uint32_t vs30;
+  double vs30;
 } VS30_point;
 
 // earthquake location data
@@ -54,28 +53,47 @@ typedef struct {
   double moment_magnitude;
 } Earthquake;
 
-// AS2008 GMPE model data point
-typedef struct {
-  double *lat;
-  double *lon;
-  uint32_t *vs30;
-  double hyp_distance; // Hypocentral distance in km
-  double f_1; // Part of the AS2008 formula responsible for the attenuation from
-              // the distance
-  double f_5; // Part of the AS2008 formula responsible for the nonlinear
-              // behavior of soils
-  double f_8; // Part of the AS2008 formula responsible for remote sources
-  double pga_1100;
-  double g; // Acceleration of gravity in fractions
-} AS2008_point;
-
 // Output data point after ALL calculations
 typedef struct {
   double *lat;
   double *lon;
   double hyp_distance; // Hypocentral distance in km
-  double g;            // Acceleration of gravity in percent
-} GMM_point;
+  double g;            // Acceleration of gravity in percent rounded to ggg.gg
+} GMM_point_pga;
+
+// AS2008 formula paramaters
+typedef struct {
+  double a1;
+  double a2;
+  double a3;
+  double a4;
+  double a5;
+  double a8;
+  double a10;
+  double a18;
+  double b;
+  double c;
+  double c1;
+  double c4;
+  double n;
+  double vlin;
+  double v1;
+  double vs30_1100;
+  double t6;
+} AS2008_parameters;
+
+// AS2008 GMPE model data point
+typedef struct {
+  double *lat;
+  double *lon;
+  double *vs30;
+  double hyp_distance; // Hypocentral distance in km
+  double f1;
+  double f5;
+  double f8;
+  double pga1100;
+  double g; // Acceleration of gravity in fractions
+} AS2008_point;
 
 //****************
 //*  PROTOTYPES  *
@@ -84,5 +102,13 @@ typedef struct {
 // Read VS30 grid file and return pointer to allocated array of VS30_points
 VS30_point *read_vs30_grid(const char *filename);
 // AS2008 GMPE
-void as2008_gmpe(const Earthquake eq, VS30_point *const vs30_grid,
-                 size_t vs30_size, GMM_point *const out_pga_data);
+// void as2008_gmpe(const Earthquake eq, VS30_point *const vs30_grid,
+//                 size_t vs30_size, GMM_point *const out_pga_data);
+GMM_point_pga *as2008_gmpe(const Earthquake eq, AS2008_parameters as2008_in,
+                           VS30_point *const vs30_grid);
+void print_as2008_parameters(const AS2008_parameters *const AS2008_conf,
+                             const char *const filename);
+void print_as2008_points(const AS2008_point *const AS2008_point_array_const,
+                         const char *const filename);
+void print_gpa_grid(const GMM_point_pga *const pga_grid_const,
+                    const char *const filename);
